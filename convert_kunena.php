5bcfff32f4ddd7add11e9377c7d9c4654ddee407
Alexey Noskov <alexey.noskov@gmail.com>
2016-02-21 23:39:34 +0300

    Initial version

---
+++ convert_kunena.php
@@ -0,0 +1,255 @@
+<?php
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+  exit;
+}
+
+include($phpbb_root_path . 'config.' . $phpEx);
+unset($dbpasswd);
+
+/**
+* $convertor_data provides some basic information about this convertor which is
+* used on the initial list of convertors and to populate the default settings
+*/
+$convertor_data = array(
+  'forum_name'  => 'Kunena x.x.x',
+  'version'   => '0.0.1',
+  'phpbb_version' => '3.1.3',
+  'author'    => '',
+  'dbms'      => $dbms,
+  'dbhost'    => $dbhost,
+  'dbport'    => $dbport,
+  'dbuser'    => $dbuser,
+  'dbpasswd'    => '',
+  'dbname'       => $dbname,
+  'table_prefix'  => '',
+  'forum_path'  => '.',
+  'author_notes'  => '',
+);
+
+/**
+* $test_file is the name of a file which is present on the source
+* forum which can be used to check that the path specified by the
+* user was correct
+*/
+$test_file = 'index.php';
+
+/**
+* $tables is a list of the tables (minus prefix) which we expect to find in the
+* source forum. It is used to guess the prefix if the specified prefix is incorrect
+*/
+$tables = array(
+  'users',
+  'kunena_users',
+  'kunena_categories',
+  'kunena_topics',
+  'kunena_messages',
+  'kunena_messages_text',
+);
+
+/**
+* If this is set then we are not generating the first page of information but getting the conversion information.
+*/
+if (!$get_info)
+{
+
+  // Overwrite maximum avatar width/height
+  //@define('DEFAULT_AVATAR_X_CUSTOM', get_config_value('avatar_max_width'));
+  //@define('DEFAULT_AVATAR_Y_CUSTOM', get_config_value('avatar_max_height'));
+
+  // Check whether the user location field exists.
+  //@define('USER_FROM_EXISTS', $helper->user_from_col_exists());
+
+
+/**
+* Description on how to use the convertor framework.
+*
+* 'schema' Syntax Description
+*   -> 'target'     => Target Table. If not specified the next table will be handled
+*   -> 'primary'    => Primary Key. If this is specified then this table is processed in batches
+*   -> 'query_first'  => array('target' or 'src', Query to execute before beginning the process
+*               (if more than one then specified as array))
+*   -> 'function_first' => Function to execute before beginning the process (if more than one then specified as array)
+*               (This is mostly useful if variables need to be given to the converting process)
+*   -> 'test_file'    => This is not used at the moment but should be filled with a file from the old installation
+*
+*   // DB Functions
+*   'distinct'  => Add DISTINCT to the select query
+*   'where'   => Add WHERE to the select query
+*   'group_by'  => Add GROUP BY to the select query
+*   'left_join' => Add LEFT JOIN to the select query (if more than one joins specified as array)
+*   'having'  => Add HAVING to the select query
+*
+*   // DB INSERT array
+*   This one consist of three parameters
+*   First Parameter:
+*             The key need to be filled within the target table
+*             If this is empty, the target table gets not assigned the source value
+*   Second Parameter:
+*             Source value. If the first parameter is specified, it will be assigned this value.
+*             If the first parameter is empty, this only gets added to the select query
+*   Third Parameter:
+*             Custom Function. Function to execute while storing source value into target table.
+*             The functions return value get stored.
+*             The function parameter consist of the value of the second parameter.
+*
+*             types:
+*               - empty string == execute nothing
+*               - string == function to execute
+*               - array == complex execution instructions
+*
+*   Complex execution instructions:
+*   @todo test complex execution instructions - in theory they will work fine
+*
+*             By defining an array as the third parameter you are able to define some statements to be executed. The key
+*             is defining what to execute, numbers can be appended...
+*
+*             'function' => execute function
+*             'execute' => run code, whereby all occurrences of {VALUE} get replaced by the last returned value.
+*                   The result *must* be assigned/stored to {RESULT}.
+*             'typecast'  => typecast value
+*
+*             The returned variables will be made always available to the next function to continue to work with.
+*
+*             example (variable inputted is an integer of 1):
+*
+*             array(
+*               'function1'   => 'increment_by_one',    // returned variable is 2
+*               'typecast'    => 'string',        // typecast variable to be a string
+*               'execute'   => '{RESULT} = {VALUE} . ' is good';', // returned variable is '2 is good'
+*               'function2'   => 'replace_good_with_bad',       // returned variable is '2 is bad'
+*             ),
+*
+*/
+  $convertor = array(
+    'test_file'       => $test_file,
+
+    'execute_first' => '
+      insert_forums();
+    ',
+
+    'execute_last'  => array(
+      'add_bots();',
+      'activate_topic_polls();',
+      'grant_permissions();',
+      'grant_forum_permissions();',
+      'grant_category_permissions();',
+      'update_folder_pm_count();',
+      'update_unread_count();',
+      'update_last_post_info();',
+    ),
+
+    'schema' => array(
+      array(
+        'target'      => USERS_TABLE,
+        'primary'   => 'users.id',
+        'autoincrement' => 'user_id',
+        'query_first' => array(
+          array('target', 'DELETE FROM ' . USERS_TABLE . ' WHERE user_id > ' . 2), // Leave anon and admin as is // <> ' . ANONYMOUS
+          array('target', $convert->truncate_statement . BOTS_TABLE),
+        ),
+
+        array('user_id',  'users.id',            ''),
+        array('username',  'users.username',            ''),
+        array('username_clean',  'users.username',            'utf8_clean_string'),
+        array('user_email',  'users.email',            ''),
+        array('user_password',   'users.password',                 ''),
+        array('user_style',       $config['default_style'],   ''),
+        array('user_permissions',   '',                 ''),
+        array('user_sig',   'kunena_users.signature',                 ''),
+        array('user_regdate',   'users.registerDate',                 ''),
+        array('user_lastvisit',   'users.lastvisitDate',              ''),
+
+
+        'left_join' => 'users LEFT JOIN kunena_users ON users.id = kunena_users.userid',
+        'where' => "users.username <> 'Anonymous'" // TODO Remove
+      ),
+
+      array(
+        'target'        => TOPICS_TABLE,
+        'query_first'   => array('target', $convert->truncate_statement . TOPICS_TABLE),
+        'primary'       => 'kunena_topics.id',
+        'autoincrement' => 'topic_id',
+
+        array('topic_id',       'kunena_topics.id',      ''),
+        array('forum_id',       'kunena_topics.category_id',      ''),
+        array('topic_title',    'kunena_topics.subject',      ''),
+        array('topic_poster',   'kunena_topics.first_post_userid',      ''),
+        array('topic_time',     'kunena_topics.first_post_time',      ''),
+        array('topic_views',    'kunena_topics.hits',      ''),
+        array('topic_visibility',  1,        ''),
+
+        array('topic_first_post_id',     'kunena_topics.first_post_id',      ''),
+        array('topic_first_poster_name', 'kunena_topics.first_post_guest_name',      ''),
+
+        array('topic_last_post_id',     'kunena_topics.last_post_id',      ''),
+        array('topic_last_post_time',   'kunena_topics.last_post_time',      ''),
+        array('topic_last_poster_id',   'kunena_topics.last_post_userid',      ''),
+        array('topic_last_poster_name', 'kunena_topics.last_post_guest_name',      ''),
+
+        array('poll_title',       'kunena_polls.title',   ''),
+        array('poll_max_options',   1,              ''),
+        array('poll_vote_change',   0,              ''),
+
+        'left_join' => 'kunena_topics LEFT JOIN kunena_polls ON kunena_polls.threadid = kunena_topics.id',
+      ),
+
+      array(
+        'target'        => POSTS_TABLE,
+        'query_first'   => array('target', $convert->truncate_statement . POSTS_TABLE),
+        'primary'       => 'kunena_messages.id',
+        'autoincrement' => 'post_id',
+
+        array('post_id',         'kunena_messages.id',      ''),
+        array('topic_id',        'kunena_messages.thread',      ''),
+        array('forum_id',        'kunena_messages.catid',      ''),
+        array('poster_id',       'kunena_messages.userid',      ''),
+        array('poster_ip',       'kunena_messages.ip',      ''),
+        array('post_time',       'kunena_messages.time',      ''),
+        array('post_subject',    'kunena_messages.subject',      ''),
+        array('post_text',       'kunena_messages_text.message', 'prepare_message'),
+        array('bbcode_uid',      '',                             'get_bbcode_uid'),
+        array('bbcode_bitfield', '',                             'get_bbcode_bitfield'),
+        array('post_visibility', 1,      ''),
+        array('post_checksum',   '',               ''),
+
+        array('post_edit_count',    'kunena_messages.modified_time',    'is_positive'),
+        array('post_edit_time',     'kunena_messages.modified_time',    array('typecast' => 'int')),
+        array('post_edit_reason',   'kunena_messages.modified_reason',               ''),
+        array('post_edit_user',     'kunena_messages.modified_by',    ''),
+
+        'left_join' => 'kunena_messages LEFT JOIN kunena_messages_text ON kunena_messages.id = kunena_messages_text.mesid',
+      ),
+
+      array(
+        'target'    => POLL_OPTIONS_TABLE,
+        'primary'   => 'kunena_polls_options.id',
+        'query_first' => array('target', $convert->truncate_statement . POLL_OPTIONS_TABLE),
+
+        array('poll_option_id',     'kunena_polls_options.id',    ''),
+        array('topic_id',           'kunena_polls.threadid',     ''),
+        array('poll_option_text',   'kunena_polls_options.text',  ''),
+        array('poll_option_total',  'kunena_polls_options.votes', ''),
+
+        'left_join' => 'kunena_polls_options LEFT JOIN kunena_polls ON kunena_polls_options.pollid = kunena_polls.id',
+      ),
+
+      array(
+        'target'    => POLL_VOTES_TABLE,
+        'query_first' => array('target', $convert->truncate_statement . POLL_VOTES_TABLE),
+
+        array('topic_id',       'kunena_polls.threadid',       ''),
+        array('poll_option_id', 'kunena_polls_users.lastvote', ''),
+        array('vote_user_id',   'kunena_polls_users.userid',   ''),
+
+        'order_by'    => 'kunena_polls_users.lasttime ASC',
+        'left_join' => 'kunena_polls_users LEFT JOIN kunena_polls ON kunena_polls_users.pollid = kunena_polls.id',
+      ),
+    ),
+  );
+}
+?>
---
+++ functions_kunena.php
@@ -0,0 +1,333 @@
+<?php
+
+function truncate_table($table)
+{
+  global $db, $convert;
+  $db->sql_query($convert->truncate_statement . $table);
+}
+
+/**
+* Calculate the left right id's for forums. This is a recursive function.
+*/
+function left_right_ids($groups, $parent_id, &$forums, &$node)
+{
+  foreach ($groups[$parent_id] as $forum_id)
+  {
+    $forums[$forum_id]['left_id'] = $node++;
+
+    if (!empty($groups[$forum_id]))
+    {
+      left_right_ids($groups, $forum_id, $forums, $node);
+    }
+
+    $forums[$forum_id]['right_id'] = $node++;
+  }
+}
+
+/**
+* Insert/Convert forums
+*/
+function insert_forums()
+{
+  global $db, $src_db, $convert, $config;
+
+  truncate_table(FORUMS_TABLE);
+
+  // Loading forum data
+
+  $sql = 'SELECT id, parent_id, name, description  FROM ' . $convert->src_table_prefix . 'kunena_categories ORDER BY id';
+  $result = $src_db->sql_query($sql);
+
+  $forums = $forum_groups = $last_topics = array();
+
+  while ($row = $src_db->sql_fetchrow($result))
+  {
+    $forums[$row['id']] = $row;
+    $forum_groups[$row['parent_id']][] = $row['id'];
+  }
+
+  $src_db->sql_freeresult($result);
+
+  $node = 1;
+  left_right_ids($forum_groups, 0, $forums, $node);
+
+  foreach ($forums as $forum_id => $row)
+  {
+    // Define the new forums sql ary
+    $sql_ary = array(
+      'forum_id'      => (int) $row['id'],
+      'forum_name'    => $row['name'],
+      'parent_id'     => (int) $row['parent_id'],
+      'forum_parents' => '',
+      'forum_desc'    => $row['description'],
+      'forum_type'    => ($row['parent_id']) ? FORUM_POST : FORUM_CAT,
+      'forum_status'  => ITEM_UNLOCKED,
+      'left_id'       => $row['left_id'],
+      'right_id'      => $row['right_id'],
+      'enable_icons'  => 1,
+
+      // Default values
+      'forum_desc_bitfield'   => '',
+      'forum_desc_options'    => 7,
+      'forum_desc_uid'      => '',
+      'forum_style'       => 0,
+      'forum_image'       => '',
+      'forum_rules'       => '',
+      'forum_rules_link'      => '',
+      'forum_rules_bitfield'    => '',
+      'forum_rules_options'   => 7,
+      'forum_rules_uid'     => '',
+      'forum_topics_per_page'   => 0,
+      'forum_posts_approved'    => 0,
+      'forum_posts_unapproved'  => 0,
+      'forum_posts_softdeleted' => 0,
+      'forum_topics_approved'   => 0,
+      'forum_topics_unapproved' => 0,
+      'forum_topics_softdeleted'  => 0,
+      'display_on_index'      => 1,
+      'enable_indexing'     => 1,
+    );
+
+    $sql = 'INSERT INTO ' . FORUMS_TABLE . ' ' . $db->sql_build_array('INSERT', $sql_ary);
+    $db->sql_query($sql);
+  }
+}
+
+function grant_permissions()
+{
+  global $db, $phpbb_root_path, $phpEx;
+
+  truncate_table(ACL_USERS_TABLE);
+  truncate_table(ACL_GROUPS_TABLE);
+/*
+  // Grab users with admin permissions
+  $sql = "SELECT uid, permissions
+    FROM {$this->src_table_prefix}adminoptions
+    WHERE uid >= 1";
+  $result = $this->src_db->sql_query($sql);*/
+
+  $admins = $founders = array();
+
+/*
+  while ($row = $this->src_db->sql_fetchrow($result))
+  {
+    $user_id = (int) $this->get_user_id($row['uid']);
+    $permissions = unserialize($row['permissions']);
+    $admins[] = $user_id;
+
+    if ($permissions['user']['admin_permissions'])
+    {
+      $founders[] = $user_id;
+    }
+  }
+  $this->src_db->sql_freeresult($result);
+
+  // We'll set the users that can manage admin permissions as founders.
+  $sql = 'UPDATE ' . USERS_TABLE . '
+    SET user_type = ' . USER_FOUNDER . "
+    WHERE " . $this->db->sql_in_set('user_id', $founders);
+  $this->db->sql_query($sql);*/
+
+  $bot_group_id = get_group_id('bots');
+
+  // Add the anonymous user to the GUESTS group, and everyone else to the REGISTERED group
+  user_group_auth('guests', 'SELECT user_id, {GUESTS} FROM ' . USERS_TABLE . ' WHERE user_id = ' . ANONYMOUS, false);
+  user_group_auth('registered', 'SELECT user_id, {REGISTERED} FROM ' . USERS_TABLE . ' WHERE user_id <> ' . ANONYMOUS . " AND group_id <> $bot_group_id", false);
+
+  if (!function_exists('group_set_user_default'))
+  {
+    include($phpbb_root_path . 'includes/functions_user.' . $phpEx);
+  }
+
+  if ($admins)
+  {
+    $auth_sql = 'SELECT user_id, {ADMINISTRATORS} FROM ' . USERS_TABLE . ' WHERE ' . $db->sql_in_set('user_id', $admins);
+    user_group_auth('administrators', $auth_sql, false);
+
+    $auth_sql = 'SELECT user_id, {GLOBAL_MODERATORS} FROM ' . USERS_TABLE . ' WHERE ' . $db->sql_in_set('user_id', $admins);
+    user_group_auth('global_moderators', $auth_sql, false);
+
+    // Set the admin group as their default group.
+    group_set_user_default(get_group_id('administrators'), $admins);
+  }
+
+  // Assign permission roles and other default permissions
+
+  // guests having u_download and u_search ability
+  $db->sql_query('INSERT INTO ' . ACL_GROUPS_TABLE . ' (group_id, forum_id, auth_option_id, auth_role_id, auth_setting) SELECT ' . get_group_id('guests') . ', 0, auth_option_id, 0, 1 FROM ' . ACL_OPTIONS_TABLE . " WHERE auth_option IN ('u_', 'u_download', 'u_search')");
+
+  // administrators/global mods having full user features
+  mass_auth('group_role', 0, 'administrators', 'USER_FULL');
+  mass_auth('group_role', 0, 'global_moderators', 'USER_FULL');
+
+  // By default all converted administrators are given full access
+  mass_auth('group_role', 0, 'administrators', 'ADMIN_FULL');
+
+  // All registered users are assigned the standard user role
+  mass_auth('group_role', 0, 'registered', 'USER_STANDARD');
+  mass_auth('group_role', 0, 'registered_coppa', 'USER_STANDARD');
+
+  // Instead of administrators being global moderators we give the MOD_FULL role to global mods (admins already assigned to this group)
+  mass_auth('group_role', 0, 'global_moderators', 'MOD_FULL');
+}
+
+function grant_category_permissions()
+{
+  global $db, $auth;
+
+  $sql = 'SELECT forum_id, forum_name, parent_id, left_id, right_id
+    FROM ' . FORUMS_TABLE . '
+    ORDER BY left_id ASC';
+  $result = $db->sql_query($sql);
+
+  $categories = array();
+  $forums = array();
+  while ($row = $db->sql_fetchrow($result))
+  {
+    if ($row['parent_id'] == 0)
+    {
+      mass_auth('group_role', $row['forum_id'], 'administrators', 'FORUM_FULL');
+      mass_auth('group_role', $row['forum_id'], 'global_moderators', 'FORUM_FULL');
+      $categories[] = $row;
+    }
+    else
+    {
+      $forums[] = $row;
+    }
+  }
+  $db->sql_freeresult($result);
+
+  foreach ($categories as $row)
+  {
+    // Get the children
+    $branch = $forum_ids = array();
+
+    foreach ($forums as $key => $_row) {
+      if ($_row['left_id'] > $row['left_id'] && $_row['left_id'] < $row['right_id']) {
+        $branch[] = $_row;
+        $forum_ids[] = $_row['forum_id'];
+        continue;
+      }
+    }
+
+    if (sizeof($forum_ids)) {
+      // Now make sure the user is able to read these forums
+      $hold_ary = $auth->acl_group_raw_data(false, 'f_list', $forum_ids);
+
+      if (empty($hold_ary)) {
+        continue;
+      }
+
+      foreach ($hold_ary as $g_id => $f_id_ary) {
+        $set_group = false;
+
+        foreach ($f_id_ary as $f_id => $auth_ary) {
+          foreach ($auth_ary as $auth_option => $setting) {
+            if ($setting == ACL_YES)
+            {
+              $set_group = true;
+              break 2;
+            }
+          }
+        }
+
+        if ($set_group) {
+          mass_auth('group', $row['forum_id'], $g_id, 'f_list', ACL_YES);
+        }
+      }
+    }
+  }
+}
+
+function grant_forum_permissions()
+{
+  global $db, $auth;
+
+  $sql = 'SELECT forum_id, forum_name, parent_id, left_id, right_id
+    FROM ' . FORUMS_TABLE . '
+    WHERE parent_id <> 0';
+  $result = $db->sql_query($sql);
+
+  $forums = array();
+  while ($row = $db->sql_fetchrow($result))
+  {
+    mass_auth('group_role', $row['forum_id'], 'administrators', 'FORUM_FULL');
+    mass_auth('group_role', $row['forum_id'], 'administrators', 'MOD_FULL');
+    mass_auth('group_role', $row['forum_id'], 'global_moderators', 'FORUM_FULL');
+    mass_auth('group_role', $row['forum_id'], 'global_moderators', 'MOD_FULL');
+    $forums[] = $row;
+  }
+  $db->sql_freeresult($result);
+
+  foreach ($forums as $row) {
+    mass_auth('group_role', $row['forum_id'], 'guests', 'FORUM_READONLY');
+    mass_auth('group_role', $row['forum_id'], 'registered', 'FORUM_POLLS');
+    mass_auth('group_role', $row['forum_id'], 'registered_coppa', 'FORUM_STANDARD');
+    mass_auth('group_role', $row['forum_id'], 'bots', 'FORUM_BOT');
+    mass_auth('group_role', $row['forum_id'], 'newly_registered', 'FORUM_NEW_MEMBER');
+  }
+}
+
+function update_last_post_info()
+{
+  global $db;
+
+  $sql = 'SELECT forum_id, forum_name, parent_id, left_id, right_id
+    FROM ' . FORUMS_TABLE . '
+    WHERE parent_id <> 0';
+  $result = $db->sql_query($sql);
+
+  $forums = array();
+  while ($row = $db->sql_fetchrow($result))
+  {
+    $forums[] = $row;
+  }
+  $db->sql_freeresult($result);
+
+  foreach ($forums as $frow) {
+    $sql = 'SELECT topic_last_post_id, topic_last_poster_id
+      FROM ' . TOPICS_TABLE . ' WHERE forum_id = ' . $frow['forum_id'] . '
+      ORDER BY topic_last_post_id DESC LIMIT 1';
+    $result = $db->sql_query($sql);
+    while ($trow = $db->sql_fetchrow($result))
+    {
+      $sql = 'UPDATE ' . FORUMS_TABLE . ' SET forum_last_post_id = ' . $trow['topic_last_post_id'] . ', forum_last_poster_id = ' . $trow['topic_last_poster_id'] . '
+        WHERE forum_id = ' . $frow['forum_id'];
+      $db->sql_query($sql);
+    }
+    $db->sql_freeresult($result);
+  }
+}
+
+function prepare_message($text) {
+  global $convert;
+
+  $uid = $bitfield = $options = ''; // will be modified by generate_text_for_storage
+  $allow_bbcode = $allow_urls = $allow_smilies = true;
+
+  generate_text_for_storage($text, $uid, $bitfield, $options, $allow_bbcode, $allow_urls, $allow_smilies);
+
+  $convert->row['bbcode_uid'] = $uid;
+  $convert->row['bbcode_bitfield'] = $bitfield;
+  $convert->row['bbcode_options'] = $options;
+
+  return $text;
+}
+
+function get_bbcode_uid() {
+  global $convert;
+  return $convert->row['bbcode_uid'];
+}
+
+function get_bbcode_bitfield() {
+  global $convert;
+  return $convert->row['bbcode_bitfield'];
+}
+
+function activate_topic_polls() {
+  global $db, $convert;
+
+  $db->sql_query('UPDATE ' . TOPICS_TABLE . " SET poll_start = topic_time WHERE poll_title <> ''");
+}
+
+?>
